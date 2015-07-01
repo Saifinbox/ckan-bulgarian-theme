1c398b07045e61cccc5276445d47bdf33ce2e84a
Dimitar Dimitrov <me@ddimitrov.name>
2015-07-01 19:51:53 +0300

    Add a default config for Compass

---
+++ config/compass.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+
+http_path       = '/'
+css_dir         = 'ckanext/bulgarian_theme/fanstatic'
+sass_dir        = 'ckanext/bulgarian_theme/fanstatic'
+output_style    = :expanded
+sourcemap       = true
+line_comments   = false
+images_dir      = 'images'
+javascripts_dir = 'javascripts'
